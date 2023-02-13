ee051ae91394c683fa6ff5204de8da5d20407e5b
Bandi-Devika <123714931+Bandi-Devika@users.noreply.github.com>
2023-02-13 19:09:52 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,12 @@
+const quiz = [
+    {
+        q:'jhgk?',
+        options:['j','b','h','k'],
+        answer:0
+    },
+    {
+        q:'jhgk?',
+        options:['j','b','h','k'],
+        answer:0
+    }
+]
